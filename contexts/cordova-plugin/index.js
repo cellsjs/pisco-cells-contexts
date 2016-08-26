7bcdb0a9a67151df6b7a4a6e875978f85d3e7ba9
jesus.diaz <jesus.diaz.contractor@bbva.com>
2016-08-26 13:31:42 +0200

    refactor(cordova-plugin): use fsUtils

--- contexts/cordova-plugin/index.js
+++ contexts/cordova-plugin/index.js
@@ -1,25 +1,14 @@
 'use strict';
 
-const fs = require('fs');
+const fsUtils = require('../../lib/fsUtils');
 
-const exists = function(what) {
-  try {
-    fs.statSync(what);
-    return true;
-  } catch (e) {
-    if (e.syscall === 'stat') {
-      return false;
-    }
-    throw e;
-  }
-};
 
 module.exports = {
 
   check() {
-    return exists('www')
-      && exists('plugin.xml')
-      && !exists('app');
+    return fsUtils.exists('www')
+      && fsUtils.exists('plugin.xml')
+      && !fsUtils.exists('app');
 
   }
 
