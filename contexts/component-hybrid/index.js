646f916d4ef895da75aa8e5d0e00dcb314e25fe8
ilias.moreno.alui.contractor <ilias.moreno.alui.contractor@bbva.com>
2017-07-24 10:25:44 +0200

    feat(): component-hybrid context added
    CELLS-65

---
+++ contexts/component-hybrid/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fsUtils = require('../../lib/fsUtils');
+
+module.exports = {
+
+  check() {
+    if (fsUtils.readJSON('.piscosour/piscosour.json').context === 'component-hybrid') {
+      return true;
+    }
+    const bowerJson = fsUtils.readJSON('bower.json');
+
+    return !fsUtils.exists('package.json') && bowerJson.dependencies
+      && bowerJson.dependencies.polymer
+      && bowerJson.name
+      && bowerJson.name !== 'repo-configs'
+      && bowerJson.variants;
+  }
+
+};
