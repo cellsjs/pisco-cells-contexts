09359541559deb533b162cda00aeecc09ff33bcc
jesus.diaz <jesus.diaz@bbva.com>
2017-07-26 13:00:33 +0200

    style(contexts/hybrid-ios-app): linter issues
    [NO_ISSUE]

--- contexts/hybrid-ios-app/index.js
+++ contexts/hybrid-ios-app/index.js
@@ -7,31 +7,33 @@ module.exports = {
   check() {
     const files = fs.readdirSync('.');
 
-	//1st exists xcode project or xcode workspace
+    //1st exists xcode project or xcode workspace
     const xcodeFiles = files.filter(file => (file.endsWith('.xcodeproj') || file.endsWith('.xcworkspace')));
 
-	//2nd exists a folder inside the sources folder called 'www'
-	var projectName = 'NoName';
-	var existsWWWasDir = false;
-	
-	if (xcodeFiles.length > 0){
-		projectName = xcodeFiles[0].substr(0, xcodeFiles[0].indexOf('.'));
-		const sourcesFolder = fs.readdirSync(projectName);
-		const webFolder = sourcesFolder.filter(file => (file.startsWith('www')));
-	
-		//3rd the 'www' element is a directory
-		if (webFolder.length > 0){
-			existsWWWasDir = fs.statSync(projectName+'/www').isDirectory
-		}
-	}
-	
-	//4th exists two files called Podfile and one of them is 'Podfile.lock'
-	const podfiles = files.filter(file => (file.startsWith('Podfile')))
-	const podfileLock = podfiles.filter(file => (file.startsWith('Podfile') && file.endsWith('.lock')));
+    //2nd exists a folder inside the sources folder called 'www'
+    let projectName = 'NoName';
+    let existsWWWasDir = false;
 
-  return xcodeFiles.length > 0 && existsWWWasDir
-  				&& podfileLock.length > 0 && podfiles.length > 1;
-    
+    if (xcodeFiles.length > 0) {
+      projectName = xcodeFiles[0].substr(0, xcodeFiles[0].indexOf('.'));
+      const sourcesFolder = fs.readdirSync(projectName);
+      const webFolder = sourcesFolder.filter(file => (file.startsWith('www')));
+
+      //3rd the 'www' element is a directory
+      if (webFolder.length > 0) {
+        existsWWWasDir = fs.statSync(projectName + '/www').isDirectory;
+      }
+    }
+
+    //4th exists two files called Podfile and one of them is 'Podfile.lock'
+    const podfiles = files.filter(file => (file.startsWith('Podfile')));
+    const podfileLock = podfiles.filter(file => (file.startsWith('Podfile') && file.endsWith('.lock')));
+
+    return xcodeFiles.length > 0
+      && existsWWWasDir
+      && podfileLock.length > 0
+      && podfiles.length > 1;
+
   }
 
 };
